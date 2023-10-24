825a700ae3b8c81a063286d729d1b8f37af4395c
strophy <badfunkstripe@gmail.com>
2023-10-24 20:38:40 +1000

    fix: cannot open retimer-state/file_info.txt: No such file

--- retimer.sh
+++ retimer.sh
@@ -22,21 +22,24 @@ save_timestamps_and_hashes() {
 restore_timestamps() {
   echo "Restoring timestamps"
   pwd
-  while read -r line; do
-    echo "$line"
-    file=$(echo "$line" | awk '{print $1}')
-    mtime=$(echo "$line" | awk '{print $2}')
-    hash=$(echo "$line" | awk '{print $3}')
-    
-    if [ "$(calculate_blake3 "$file")" = "$hash" ]; then
-      echo "Restoring mtime for $file"
-      touch -d "@$mtime" "$file"
-    else
-      echo "Hash mismatch for $file"
-    fi
-  done < "$OUTPUT_FILE"
+  if [ -f "$OUTPUT_FILE" ]; then
+    while read -r line; do
+      echo "$line"
+      file=$(echo "$line" | awk '{print $1}')
+      mtime=$(echo "$line" | awk '{print $2}')
+      hash=$(echo "$line" | awk '{print $3}')
+      
+      if [ "$(calculate_blake3 "$file")" = "$hash" ]; then
+        echo "Restoring mtime for $file"
+        touch -d "@$mtime" "$file"
+      else
+        echo "Hash mismatch for $file"
+      fi
+    done < "$OUTPUT_FILE"
+  else
+    echo "Output file $OUTPUT_FILE does not exist. Nothing to restore."
+  fi
 }
-
 
 mkdir -p "$OUTPUT_DIR"
 case "$1" in
